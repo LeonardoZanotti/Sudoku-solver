fae2fd81f307d4eeaac9e99fb23e4de714fdc2f0
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-05-31 17:58:10 -0300

    btn function

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const squares = 81;
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement('input');
   inputElement.setAttribute('type', 'number');
-  inputElement.setAttribute('min', 0);
+  inputElement.setAttribute('min', 1);
   inputElement.setAttribute('max', 9);
   puzzleBoard.appendChild(inputElement);
 }
+
+const joinValues = () => {
+  const inputs = document.querySelectorAll('input');
+  const valuesArray = [];
+  inputs.forEach((input) => valuesArray.push(input.value ? input.value : '.'));
+  console.log(valuesArray);
+  return valuesArray.join('');
+};
+
+solveBtn.addEventListener('click', joinValues);
