70e1610496b3973576f28d6dce824b5223c57df2
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-05-31 18:14:25 -0300

    have to create a server to store apiKey

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const puzzleBoard = document.querySelector('#puzzle');
 const solveBtn = document.querySelector('#solve-btn');
 const squares = 81;
-
+require('dotenv').config();
+console.log(process.env.apiKey);
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement('input');
   inputElement.setAttribute('type', 'number');
@@ -18,4 +19,27 @@ const joinValues = () => {
   return valuesArray.join('');
 };
 
+const solveByApi = () => {
+  const axios = require('axios');
+
+  const options = {
+    method: 'POST',
+    url: 'https://solve-sudoku.p.rapidapi.com/',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com',
+      'X-RapidAPI-Key': '',
+    },
+    data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}',
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 solveBtn.addEventListener('click', joinValues);
