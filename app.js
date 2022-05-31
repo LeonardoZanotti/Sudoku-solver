4e73f2f98bac0b9ed2fb5b88fe13bbf319fe8125
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-05-31 19:05:45 -0300

    sudoku solver done

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ for (let i = 0; i < squares * squares; i++) {
   puzzleBoard.appendChild(inputElement);
 }
 
+const populateValues = (result) => {
+  const inputs = document.querySelectorAll('input');
+  if (result.solvable) {
+    let row = 0,
+      col = 0;
+    inputs.forEach((input) => {
+      input.value = result.solution[row][col++];
+      if (col > squares - 1) {
+        row++;
+        col = 0;
+      }
+    });
+  }
+};
+
 const joinValues = () => {
   const inputs = document.querySelectorAll('input');
   let valuesArray = [];
@@ -24,9 +39,43 @@ const joinValues = () => {
   return valuesBoard;
 };
 
+function isValid(board, row, col, k) {
+  for (let i = 0; i < 9; i++) {
+    const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+    const n = 3 * Math.floor(col / 3) + (i % 3);
+    if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sudokuSolver(data) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (data[i][j] == '.') {
+        for (let k = 1; k <= 9; k++) {
+          if (isValid(data, i, j, k)) {
+            data[i][j] = `${k}`;
+            if (sudokuSolver(data)) {
+              return true;
+            } else {
+              data[i][j] = '.';
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 const solveSudoku = () => {
   const board = joinValues();
-  return { solvable: true, solution: '' };
+  const result = sudokuSolver(board) ? { solvable: true, solution: board } : { solvable: false };
+  console.log(result);
+  populateValues(result);
 };
 
 solveBtn.addEventListener('click', solveSudoku);
