058c262d0c507bad54fc4e5d8e9a19d0e0660c54
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-05-31 18:44:59 -0300

    the api is paid, so i will do the hard work myself

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const puzzleBoard = document.querySelector('#puzzle');
 const solveBtn = document.querySelector('#solve-btn');
-const squares = 81;
-require('dotenv').config();
-console.log(process.env.apiKey);
-for (let i = 0; i < squares; i++) {
+const squares = 9;
+
+for (let i = 0; i < squares * squares; i++) {
   const inputElement = document.createElement('input');
   inputElement.setAttribute('type', 'number');
   inputElement.setAttribute('min', 1);
@@ -13,33 +12,21 @@ for (let i = 0; i < squares; i++) {
 
 const joinValues = () => {
   const inputs = document.querySelectorAll('input');
-  const valuesArray = [];
-  inputs.forEach((input) => valuesArray.push(input.value ? input.value : '.'));
-  console.log(valuesArray);
-  return valuesArray.join('');
+  let valuesArray = [];
+  const valuesBoard = [];
+  inputs.forEach((input, i) => {
+    valuesArray.push(input.value ? input.value : '.');
+    if ((i + 1) % squares === 0) {
+      valuesBoard.push(valuesArray);
+      valuesArray = [];
+    }
+  });
+  return valuesBoard;
 };
 
-const solveByApi = () => {
-  const axios = require('axios');
-
-  const options = {
-    method: 'POST',
-    url: 'https://solve-sudoku.p.rapidapi.com/',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com',
-      'X-RapidAPI-Key': '',
-    },
-    data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}',
-  };
-
-  axios
-    .request(options)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+const solveSudoku = () => {
+  const board = joinValues();
+  return { solvable: true, solution: '' };
 };
-solveBtn.addEventListener('click', joinValues);
+
+solveBtn.addEventListener('click', solveSudoku);
