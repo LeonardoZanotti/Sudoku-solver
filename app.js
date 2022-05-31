bc6b1594c50081db096dc67c4d8db91d0067b092
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-05-31 17:53:16 -0300

    basic dinamic board style

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const squares = 81;
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement('input');
   inputElement.setAttribute('type', 'number');
-  inputElement.setAttribute('min', '0');
-  inputElement.setAttribute('max', '9');
+  inputElement.setAttribute('min', 0);
+  inputElement.setAttribute('max', 9);
   puzzleBoard.appendChild(inputElement);
 }
